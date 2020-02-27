42459a33800b8882b60ea15855da2adc181e4f9f
Natali Maevskaya <natali.maevskaya@gmail.com>
2020-02-27 13:58:46 +0300

    lesson1 task is done

---
+++ data/dataMenu.js
@@ -0,0 +1,17 @@
+const menuListSet = [{
+        id: 1,
+        title: "Dashboard"
+    },
+    {
+        id: 2,
+        title: "Users"
+    },
+    {
+        id: 3,
+        title: "Products"
+    },
+    {
+        id: 4,
+        title: "Locations"
+    }
+];
---
+++ script/script.js
@@ -0,0 +1,149 @@
+const toolbar = {
+    view: "toolbar",
+    id: "toolbar",
+    paddingX: 12,
+    css: "webix_dark",
+    elements: [{
+            view: "label",
+            label: "My App",
+        },
+        {},
+        {
+            view: "button",
+            type: "icon",
+            icon: "wxi-user",
+            label: "Profile",
+            width: 90,
+            css: "webix_transparent"
+        }
+    ]
+};
+const menuList = {
+    view: "list",
+    id: "list",
+    template: "#title#",
+    scroll: false,
+    css: "webix_layout_clean",
+    data: menuListSet
+};
+const menuLink = {
+    view: "button",
+    type: "icon",
+    icon: "wxi-check",
+    label: "Connected",
+    css: "btn-green webix_transparent"
+};
+const menu = {
+    maxWidth: 200,
+    minWidth: 100,
+    type: "clean",
+    rows: [
+        menuList,
+        {},
+        menuLink
+    ],
+    css: "menu"
+
+};
+const datatable = {
+    view: "datatable",
+    id: "datatable",
+    autoConfig: true,
+    scroll: "y",
+    columns: [{
+            id: "title",
+            header: "Title",
+            fillspace: 3
+        },
+        {
+            id: "year",
+            header: "Year",
+        },
+        {
+            id: "votes",
+            header: "Votes",
+            fillspace: 1
+        },
+        {
+            id: "rating",
+            header: "Rating",
+        },
+        {
+            id: "rank",
+            header: "Rank",
+        }
+    ],
+    data: smallFilmSet
+};
+const formButtons = {
+    autoConfig: true,
+    cols: [{
+            view: "button",
+            value: "Add new",
+            css: "webix_primary"
+        },
+        {
+            width: 10
+        },
+        {
+            view: "button",
+            value: "Clear",
+
+        }
+    ]
+};
+const form = {
+    view: "form",
+    width: 300,
+    paddingX: 20,
+    elements: [
+        {
+            template: "Edit films",
+            type: "section"
+        },
+        {
+            view: "text",
+            label: "Title"
+        },
+        {
+            view: "text",
+            label: "Year"
+        },
+        {
+            view: "text",
+            label: "Rating"
+        },
+        {
+            view: "text",
+            label: "Votes"
+        },
+        formButtons,
+        {}
+    ]
+};
+const footer = {
+    template: "The software is provided by <a href='https://webix.com/'>https://webix.com/</a>. All rights reserved (c)",
+    height: 38,
+    css: "footer"
+};
+const resizer = {
+    view: "resizer"
+};
+const main = {
+    cols: [
+        menu,
+        resizer,
+        datatable,
+        form
+    ]
+};
+
+webix.ready(function () {
+    webix.ui({
+        rows: [
+            toolbar,
+            main,
+            footer
+        ]
+    });
+});
